c79da40c3cdb41e86e8161f083af236e9a320232
Udi Barak <udibarak@gmail.com>
2022-01-16 17:10:30 +0200

    few optimizations

--- app.py
+++ app.py
@@ -7,12 +7,7 @@ global difficulty, decision
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-
-@app.route("/index.html")
-def upload_file():
-    return render_template('upload.html')
-
-@app.route("/hello.html")
+@app.route("/")
 def hello():
     return render_template('hello.html')
 
@@ -34,7 +29,6 @@ def game_chooser():
             return render_template('hello.html')
 
 @app.route("/choosedifficulty", methods=['POST'])
-
 def difficulty_chooser():
     if request.method == 'POST':
         if 'Memory_Game' in request.form:
@@ -46,9 +40,6 @@ def difficulty_chooser():
         if 'Currency Roulette' in request.form:
             game_name = 'Currency Roulette'
             return render_template('choosedifficulty.html', game_name=game_name)
-
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001, debug=True)
