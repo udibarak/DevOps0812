d0ac16b1ccb256976b50c32e37f0ba93b01d144b
Udi Barak <udibarak@gmail.com>
2022-02-23 23:54:54 +0200

    fix

--- app.py
+++ app.py
@@ -30,13 +30,15 @@ def difficulty_chooser():
         if 'Memory_Game' in request.form:
             game_name = 'Memory Game'
             return render_template('choosedifficulty.html', game_name=game_name)
-        if 'Guess Game' in request.form:
+        if 'Guess_Game' in request.form:
             game_name = 'Guess Game'
             return render_template('choosedifficulty.html', game_name=game_name)
-        if 'Currency Roulette' in request.form:
+        if 'Currency_Roulette' in request.form:
             game_name = 'Currency Roulette'
             return render_template('choosedifficulty.html', game_name=game_name)
-
+@app.route("/guess", method=['POST']
+    if request.method == 'POST':
+        if
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001, debug=True)
 
